2db7f72bf7dea88e408190fb511aee381653ae98
Kazuya Ogawa <jiashouyexiaochuan@gmail.com>
2025-12-08 08:15:31 +0900

    Add Spring Boot backend project

---
+++ src/main/java/com/kazuya/teach_learning_backend/TeachLearningBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kazuya.teach_learning_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachLearningBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachLearningBackendApplication.class, args);
+	}
+
+}
